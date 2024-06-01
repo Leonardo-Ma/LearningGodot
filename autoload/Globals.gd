8d3e169a655b6cc96f4fc178fb89be72331442ed
Leonardo <filho.leonardo@hotmail.com>
2024-05-31 23:33:16 -0300

    Removed game manager and placed logic inside globals

--- autoload/Globals.gd
+++ autoload/Globals.gd
@@ -6,3 +6,11 @@ var coinsCollected = 0
 var playerHealth = 100
 var playerDamage = 10
 var playerSpeed = 130.0
+var coins = 0
+
+func _ready():
+	randomize()
+	soundtrack_player.play_theme(soundtrackPlayerClass.PlaylistTheme.PEACE)
+	
+func add_coin():
+	coins += 1
--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,9 +1,8 @@
 extends Area2D
 
-@onready var game_manager = %GameManager
 @onready var animation_player = $AnimationPlayer
 
 func _on_body_entered(body):
-	game_manager.add_coin()
+	Globals.add_coin()
 	animation_player.play("pickup_animation")
 	Globals.coinsCollected += 1
